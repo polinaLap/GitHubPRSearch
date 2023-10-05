a78bd19abca5de33e09aa501ed71bb0e3564dc04
polinal <lapanya17@gmail.com>
2023-10-05 18:28:40 +0100

    created default mvc project

---
+++ GitHubPRSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHubPRSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
